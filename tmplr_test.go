58f5c9e2c5e5ec8f1e012adc42ac2c3a17b05325
karantin2020 <karantin3030@mail.ru>
2018-03-13 23:43:40 +0500

    Fix test files

--- tmplr_test.go
+++ tmplr_test.go
@@ -5,4 +5,4 @@ package main
 //go:generate ./tmplr -t ./tmpls/SGBson -g
 //go:generate ./tmplr -t SGBson -o ./tmp/IntStr -p tmp str:Int lst:nums
 //go:generate ./tmplr -t SGBson --append -o ./tmp/IntStr str:CondKind lst:conds
-//go:generate ./tmplr -t SGBson -a -o ./tmp/IntStr str:CondDo lst:cords
+//go:generate ./tmplr -t SGBson -a -o IntStr -p tmp str:CondDo lst:cords
